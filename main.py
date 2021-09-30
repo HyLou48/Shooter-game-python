f1a6d158274b53978b87d9ff3b055e236082f89d
HyLou48 <hobimpifaliana@gmail.com>
2021-09-30 17:54:58 +0100

    'Create player and movement of the player'

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from game import Game
+from player import Player
 
 #initialize pygame
 pygame.init()
@@ -10,11 +12,28 @@ screen = pygame.display.set_mode((1080, 720))
 #importer de charger l'arrière plan de notre jeu
 background = pygame.image.load('Assets/bg.jpg')
 
+#charger notre joueur
+player = Player()
+
+#charger notre jeu
+game = Game()
+
+
 #game loop
 running = True
 while running:
     # appliquer l'arriere plan de notre jeu
     screen.blit(background, (0, -200))
+
+    #appliquer l'image de mon joueur
+    screen.blit(game.player.image, game.player.rect)
+
+    # verifier si le joueur souhaite aller à gauche ou à droite
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
+        game.player.move_right()
+    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
+        game.player.move_left()
+
     # mettre à jour l'ecran
     pygame.display.flip()
 
@@ -22,3 +41,10 @@ while running:
         if event.type == pygame.QUIT:
             running == False
             pygame.quit()
+            print("Fermeture du jeu")
+
+        #detecter si un joueur lache une touche du clavier
+        elif event.type == pygame.KEYDOWN:
+            game.pressed[event.key] = True
+        elif event.type == pygame.KEYUP:
+            game.pressed[event.key] = False
